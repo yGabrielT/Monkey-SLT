e5b1fe5fa8385b28d6462f8365a186b65bff3326
yGabrielT <gtrindade380@gmail.com>
2023-08-18 08:45:45 -0300

    Inicios dos arquivos do unity

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private Camera camera;
+    private Vector3 mousePos;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
