2e953b64a87933168de4f1cee813b9d538ef4f11
yGabrielT <gtrindade380@gmail.com>
2023-08-20 17:53:34 -0300

    Inicio da progamação do geramento aleatorio

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    //1- Top
+    //2- Right
+    //3 - Bottom
+    //4 - left
+
+    void Update()
+    {
+        if(openingDirection == 1)
+        {
+
+        }
+        if (openingDirection == 2)
+        {
+
+        }
+        if (openingDirection == 3)
+        {
+
+        }
+        if (openingDirection == 4)
+        {
+
+        }
+    }
+}
